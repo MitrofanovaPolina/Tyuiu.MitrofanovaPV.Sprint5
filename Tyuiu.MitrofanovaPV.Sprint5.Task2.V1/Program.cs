4f5d5c6fc67c1254e18561508dd371765cf5a285
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-26 02:39:10 +0500

    Task2

--- Tyuiu.MitrofanovaPV.Sprint5.Task2.V1/Program.cs
+++ Tyuiu.MitrofanovaPV.Sprint5.Task2.V1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         DataService ds = new DataService();
-        int[,] mtr = new int[3, 3] { {6,9,4 }, {7,2,4 }, {4,8,3 } };
+        int[,] mtr = new int[3, 3] { { 6, 9, 4 }, { 7, 2, 4 }, { 4, 8, 3 } };
         int rows = mtr.GetUpperBound(0) +1;
         int col = mtr.Length / rows;
         string res = ds.SaveToFileTextData(mtr);
