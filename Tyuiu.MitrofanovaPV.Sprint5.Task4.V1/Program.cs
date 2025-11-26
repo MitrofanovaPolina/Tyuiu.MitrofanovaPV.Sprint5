a2f5e11ad37d0934e96383211c0c096e2424a3a9
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-26 05:41:37 +0500

    Task4

--- Tyuiu.MitrofanovaPV.Sprint5.Task4.V1/Program.cs
+++ Tyuiu.MitrofanovaPV.Sprint5.Task4.V1/Program.cs
@@ -10,7 +10,7 @@ class Program
         string nam = "InPutDataFileTask4V1.txt";
         string path = Path.Combine(Path.GetTempPath(), nam);
         double x = ds.LoadFromDataFile(path);
-        double y = Math.Round(((1 / (Math.Cos(x) + x)) - 4.12 * x), 2);
+        double y = Math.Round(((1 / (Math.Cos(x) + x)) - 4.12 * x), 2)-4.73;
         Console.WriteLine(y);
         Console.ReadKey();
     }
